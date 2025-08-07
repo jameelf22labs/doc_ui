25ee60671f68a7cbf32f7856a9fc66daf7a7ce06
jameelf22labs <jameel.m@f22labs.com>
2025-08-07 18:16:13 +0530

    fix socket

--- src/service/socket.ts
+++ src/service/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  import.meta.env.VITE_API_URL || "http://localhost:9090"
+  import.meta.env.VITE_API_URL || "https://sde-intern-resources-node-io-api.vaacdq.easypanel.host"
 );
